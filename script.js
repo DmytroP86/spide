d0a522e0d5881ceb54c108fa19056cfe660d70f1
DmytroP86 <32996585+DmytroP86@users.noreply.github.com>
2017-10-22 11:04:10 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,53 @@
+var user={
+	name: "Dmytro",
+	lastName: "Poluban",
+	sayHello: function (){
+		console.log('Hello');
+	},
+goToCar: function (){
+	car['user']= user.name;
+}
+
+
+};
+alert('Hello');
+console.log('hello');
+var car={
+	make: 'Mersedes',
+	model: 'SLK',
+    color: 'Black',	
+	
+	
+};
+user.goToCar();
+console.log(car);
+var car2= {
+	make: 'Toyota',
+	model: 'Camry',
+    color: 'White',
+
+};
+console.log(car2);
+var cat = {}
+
+cat.run = function(n) {
+
+    alert("Пробежал "+n+" метров!")
+
+}
+
+cat.run(5)  
+
+cat.run(7)  
+console.log(cat);
+var tiger=  {};
+	tiger.speak = function() {
+		tiger.speaks("RRR")
+		alert ('RRR')
+	};
+	alert ('RRR');
+	console.log(tiger);
+
+
+
+
